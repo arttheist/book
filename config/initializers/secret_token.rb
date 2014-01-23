58ace2dc5b1b8595089870e6d4f4b13cb3ecd5c8
Rene Vos <re.vos@arttheisten.de>
2014-01-23 20:23:18 +0100

    setting up the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = 'c394a00f88c9e7f84b32e947072c110b06547ff2cf5df47b5b82fdf9c4731ec91206262dbf33233ae59d9e525e81dac85b63007c7c4a579b7f78cc53a44b3ec8'
